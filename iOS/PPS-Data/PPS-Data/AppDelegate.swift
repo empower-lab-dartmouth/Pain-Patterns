d194528ff85f98e4714a5f018db1581acede2403
Joshua Ren <jr@DNab404678.stanford.edu>
2019-08-23 14:15:11 -0700

    developing app delegate

--- iOS/PPS-Data/PPS-Data/AppDelegate.swift
+++ iOS/PPS-Data/PPS-Data/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Foundation
+import UserNotifications
 import CoreMotion
 
 
@@ -27,6 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //  Default is set up at the minimum background fetch
         UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
         
+        // Initialize notification capabilities
+        registerForPushNotifications()
+        createPushNotifications()
+        listScheduledNotifications()
         print("Setup complete.")
         
         return true
@@ -65,4 +70,79 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //        self.manager?.syncAllSensors()
     }
     
+    // Called in order to get permissions to send notificatoins
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) {
+                [weak self] granted, error in
+                
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+        }
+    }
+    
+    // Called to make sure notifications are allowed
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    // Helper function for creating notifications
+    func helpCreateNotification(contentTitle: String, contentBody: String, dateHour: Int, dateMinutes: Int) -> UNNotificationRequest {
+        // notification content details
+        let content = UNMutableNotificationContent()
+        content.title = contentTitle
+        content.body = contentBody
+        content.sound = .default
+        
+        // notification sending times: per day
+        var date = DateComponents()
+        date.hour = dateHour
+        date.minute = dateMinutes
+        
+        let uuidString = UUID().uuidString                                                  // string representation of the NSUUID object
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)      // repeats: true will repeat sending the notification                                                                                     at the specified time
+        return UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger) // return a basket containing notification details
+    }
+    
+    // Called in order to actually send notifications
+    // Details of the content and when to send are specified here
+    // current implementation sends X notifications per day
+    func createPushNotifications() {
+        //  change the values for contentTitle, contentBody, dateHour, dateMinutes to alter the content of the notification and when it gets sent
+        //  create more requests by copy pasting to requests array if needed
+        let requests = [
+            helpCreateNotification(contentTitle: "ESM Survey", contentBody: "Time to take a survey! :)", dateHour: 0, dateMinutes: 1),
+            helpCreateNotification(contentTitle: "ESM Survey", contentBody: "Time to take a survey! :)", dateHour: 6, dateMinutes: 1),
+            helpCreateNotification(contentTitle: "ESM Survey", contentBody: "Time to take a survey! :)", dateHour: 12, dateMinutes: 1),
+            helpCreateNotification(contentTitle: "ESM Survey", contentBody: "Time to take a survey! :)", dateHour: 18, dateMinutes: 1),
+        ]
+        // Schedule the request with the APN service by adding them to the UNUserNotificationCenter.current()
+        for request in requests {
+            UNUserNotificationCenter.current().add(request)  { (error) in
+                if let error = error {
+                    print("error in PPS reminder: \(error.localizedDescription)")
+                }
+            }
+        }
+        
+    }
+    
+    // prints all current pending notifications
+    func listScheduledNotifications() {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { notifications in
+            var count = 0
+            for notification in notifications {
+                print(notification)
+                count += 1
+            }
+            print("\nnotification count: ", count, "\n")
+        }
+    }
 }
